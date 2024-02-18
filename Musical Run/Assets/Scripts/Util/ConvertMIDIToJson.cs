f1e8e70c0f7b2d287d7e5235cf3d68997629c08f
Thiago de Souza Farias <tsf@cin.ufpe.br>
2024-02-18 17:24:10 -0300

    Using sustain pedal data

--- Musical Run/Assets/Scripts/Util/ConvertMIDIToJson.cs
+++ Musical Run/Assets/Scripts/Util/ConvertMIDIToJson.cs
@@ -14,12 +14,62 @@ public class ConvertMIDIToJson : MonoBehaviour
     private IEnumerable<Note> notes;
     private IEnumerable<Note> accNotes;
 
+    private bool isSustainPressed = false;
+
+    List<Pedal> pedalTimes = new();
+
+    public class Pedal
+    {
+        public int pressPedalTime;
+        public int releasePedalTime;
+    }
+
     [ContextMenu("Convert MIDI to JSON")]
     private void Convert()
     {
+        GetPedal();
         ReadFromMIDI();
         WriteJSON();
         Debug.Log("Conversion done!");
+    }
+
+    [ContextMenu("Pedal data")]
+    private void GetPedal()
+    {
+        long currentTime = 0;
+        MidiFile accompaniment = MidiFile.Read(pathToAccompanimentMIDI);
+        Pedal pedal = new();
+        foreach (var trackChunk in accompaniment.GetTrackChunks())
+        {
+            foreach (var midiEvent in trackChunk.Events)
+            {
+                currentTime += midiEvent.DeltaTime; // Adiciona o tempo delta ao tempo atual
+
+                if (midiEvent is ControlChangeEvent controlChangeEvent)
+                {
+                    if (controlChangeEvent.ControlNumber == 64)
+                    {
+                        
+                        // É um evento de controle do pedal (CC64)
+                        if (controlChangeEvent.ControlValue > 63) // Valor típico para pedal pressionado
+                        {
+                            pedal = new();
+                            pedal.pressPedalTime = (int)currentTime;
+                        }
+                        else // Valor típico para pedal solto
+                        {
+                            pedal.releasePedalTime = (int)currentTime;
+                            pedalTimes.Add(pedal);
+                        }
+                    }
+                }
+            }
+        }
+
+        foreach (var pedalTime in pedalTimes)
+        {
+            Debug.Log($"Pres time: {pedalTime.pressPedalTime} | Release Time: {pedalTime.releasePedalTime}");
+        }
     }
 
     void ReadFromMIDI()
@@ -29,6 +79,8 @@ public class ConvertMIDIToJson : MonoBehaviour
 
         MidiFile accompaniment = MidiFile.Read(pathToAccompanimentMIDI);
         accNotes = accompaniment.GetNotes();
+
+        
     }
 
     void WriteJSON()
@@ -36,12 +88,13 @@ public class ConvertMIDIToJson : MonoBehaviour
         // Melody
         NoteInfo[] melodyNotes = CreateArray(notes);
         string melodyJson = JsonHelper.ToJson(melodyNotes, true);
-        File.WriteAllText(Application.dataPath + "/Audio/Json Files/" + musicName + "_melody.json", melodyJson);
+        File.WriteAllText(Application.dataPath + "/Music/Json Files/" + musicName + "_melody.json", melodyJson);
+
 
         // Accompaniment
         NoteInfo[] accompanimentNotes = CreateArray(accNotes);
         string accompanimentJson = JsonHelper.ToJson(accompanimentNotes, true);
-        File.WriteAllText(Application.dataPath + "/Audio/Json Files/" + musicName + "_accompaniment.json", accompanimentJson);
+        File.WriteAllText(Application.dataPath + "/Music/Json Files/" + musicName + "_accompaniment.json", accompanimentJson);
 
     }
 
@@ -53,6 +106,14 @@ public class ConvertMIDIToJson : MonoBehaviour
 
         foreach (Note note in notes)
         {
+            foreach(Pedal pedalTime in pedalTimes)
+            {
+                if (note.EndTime >= pedalTime.pressPedalTime && note.EndTime <= pedalTime.releasePedalTime)
+                {
+                    note.Length = (pedalTime.releasePedalTime - note.Time) - 2;
+                }
+            }
+
             NoteInfo currentNote = new()
             {
                 Octave = note.Octave,
